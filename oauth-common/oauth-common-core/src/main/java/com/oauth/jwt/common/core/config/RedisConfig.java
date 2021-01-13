2f4cb08a7be0245bb64582c16201cba45f76548c
syl <Yie192xin>
2021-01-13 18:35:31 +0800

    1.13

--- oauth-common/oauth-common-core/src/main/java/com/oauth/jwt/common/core/config/RedisConfig.java
+++ oauth-common/oauth-common-core/src/main/java/com/oauth/jwt/common/core/config/RedisConfig.java
@@ -1,11 +1,17 @@
 package com.oauth.jwt.common.core.config;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.SerializationException;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.nio.charset.Charset;
 
 @Log4j2
 @Configuration
@@ -25,5 +31,37 @@ public class RedisConfig {
         redisTemplate.setHashValueSerializer(fastJsonRedisSerializer);
         redisTemplate.afterPropertiesSet();
         return redisTemplate;
+    }
+
+    /**
+     * json序列化 支持自定义
+     */
+    private class FastJsonRedisSerializer<T> implements RedisSerializer<T> {
+
+        public final Charset DEFAULT_CHARSET = Charset.forName("UTF-8");
+
+        private Class<T> clazz;
+
+        public FastJsonRedisSerializer(Class<T> clazz) {
+            super();
+            this.clazz = clazz;
+        }
+
+        @Override
+        public byte[] serialize(T t) throws SerializationException {
+            if (t == null) {
+                return new byte[0];
+            }
+            return JSON.toJSONString(t, SerializerFeature.WriteClassName).getBytes(DEFAULT_CHARSET);
+        }
+
+        @Override
+        public T deserialize(byte[] bytes) throws SerializationException {
+            if (bytes == null || bytes.length <= 0) {
+                return null;
+            }
+            String str = new String(bytes, DEFAULT_CHARSET);
+            return JSON.parseObject(str, clazz);
+        }
     }
 }
